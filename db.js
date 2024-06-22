5229f77f6d3dc41e6aea85e81f87c8d3925377c5
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2024-06-22 19:58:07 +0000

    Refactoring...
    ...

--- db.js
+++ db.js
@@ -18,6 +18,15 @@ const pool = mysql.createPool({
     keepAliveInitialDelay: 0,
 });
 
+const tables_names = {
+    "users_table": "Users",
+    "messages_table": "Messages",
+    "friendships_table": "Friendships",
+    "notifications_table": "Notifications",
+    "codes_table": "Codes",
+    "templates_table": "Templates"
+}
+
 async function doQuery(query = "") {
     return new Promise(async (resolve, reject) => {
         const connection = await pool.getConnection();
@@ -36,23 +45,23 @@ async function doQuery(query = "") {
 
 module.exports = {
     test: () => doQuery(`SELECT NOW();`),
-    getUser: (u) => doQuery(`SELECT id, username, activated FROM ${config.users_table} WHERE username='${u}';`),
-    getUserByHS: (l, p) => doQuery(`SELECT username, activated FROM ${config.users_table} WHERE login='${l}' AND password='${p}';`),
-    getUserById: (i) => doQuery(`SELECT username FROM ${config.users_table} WHERE id=${i};`),
+    getUser: (u) => doQuery(`SELECT id, username, activated FROM ${tables_names.users_table} WHERE username='${u}';`),
+    getUserByHS: (l, p) => doQuery(`SELECT username, activated FROM ${tables_names.users_table} WHERE login='${l}' AND password='${p}';`),
+    getUserById: (i) => doQuery(`SELECT username FROM ${tables_names.users_table} WHERE id=${i};`),
     getMessages: (sender, recipient) => new Promise((resolve, reject) => {
-        doQuery(`SELECT id FROM ${config.users_table} WHERE username='${sender}'`)
+        doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${sender}'`)
             .then(sender_id => {
                 if (sender_id[0] === undefined) {
                     reject("Given sender not found...")
                     return
                 }
-                doQuery(`SELECT id FROM ${config.users_table} WHERE username='${recipient}'`)
+                doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${recipient}'`)
                     .then(recipient_id => {
                         if (recipient_id[0] === undefined) {
                             reject("Given recipient not found...")
                             return
                         }
-                        doQuery(`SELECT * FROM ${config.messages_table} WHERE sender_id = ${sender_id[0].id} AND recipient_id=${recipient_id[0].id}`)
+                        doQuery(`SELECT * FROM ${tables_names.messages_table} WHERE sender_id = ${sender_id[0].id} AND recipient_id=${recipient_id[0].id}`)
                             .then((rows) => {
                                 resolve(rows)
                             }).catch((err) => {
@@ -62,15 +71,15 @@ module.exports = {
                     })
             })
     }),
-    getCode: (u_id, t = 'T') => doQuery(`SELECT code FROM ${config.codes_table} WHERE user_id=${u_id} AND temporary='${t}';`),
+    getCode: (u_id, t = 'T') => doQuery(`SELECT code FROM ${tables_names.codes_table} WHERE user_id=${u_id} AND temporary='${t}';`),
     getNotification: (u, t) => new Promise((resolve, reject) => { //TODO TO DO SYMETRIC, BUT MUSTN'T HAVE
-        doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}';`)
+        doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}';`)
             .then(result => {
                 if (result[0] === undefined) {
                     reject("Given user or type not found...")
                     return
                 }
-                doQuery(`SELECT * FROM ${config.notifications_table} WHERE (user_1_id=${result[0].id} OR user_2_id=${result[0].id}) AND type='${t}';`)
+                doQuery(`SELECT * FROM ${tables_names.notifications_table} WHERE (user_1_id=${result[0].id} OR user_2_id=${result[0].id}) AND type='${t}';`)
                     .then((rows) => {
                         resolve(rows)
                     }).catch((err) => {
@@ -80,13 +89,13 @@ module.exports = {
             })
     }),
     getNotifications: (u) => new Promise((resolve, reject) => {
-        doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}'`)
+        doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}'`)
             .then(result => {
                 if (result[0] === undefined) {
                     reject("Given user not found...")
                     return
                 }
-                doQuery(`SELECT * FROM ${config.notifications_table} WHERE user_2_id=${result[0].id};`)
+                doQuery(`SELECT * FROM ${tables_names.notifications_table} WHERE user_2_id=${result[0].id};`)
                     .then((rows) => {
                         resolve(rows)
                     }).catch((err) => {
@@ -96,18 +105,18 @@ module.exports = {
             })
     }),
     getFriends: (u) => new Promise((resolve, reject) => {
-        doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}';`)
+        doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}';`)
             .then(result => {
                 if (result[0] === undefined) {
                     reject("Given user not found...")
                     return
                 }
-                doQuery(`SELECT user_1_id, user_2_id FROM ${config.friendships_table} WHERE user_1_id=${result[0].id} OR user_2_id=${result[0].id};`)
+                doQuery(`SELECT user_1_id, user_2_id FROM ${tables_names.friendships_table} WHERE user_1_id=${result[0].id} OR user_2_id=${result[0].id};`)
                     .then(ids => {
                         if (ids[0] === undefined)
                             resolve([]);
                         else
-                            doQuery(`SELECT username FROM ${config.users_table} WHERE id IN (${ids.map(x => x.user_1_id)},${ids.map(x => x.user_2_id)});`)
+                            doQuery(`SELECT username FROM ${tables_names.users_table} WHERE id IN (${ids.map(x => x.user_1_id)},${ids.map(x => x.user_2_id)});`)
                                 .then((rows) => {
                                     resolve(rows)
                                 }).catch((err) => {
@@ -118,13 +127,13 @@ module.exports = {
             })
     }),
     getFriendship: (u, uu) => new Promise((resolve, reject) => {
-        doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}';`)
+        doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}';`)
             .then(r => {
                 if (r[0] === undefined) {
                     reject("Given user not found...")
                     return
                 }
-                doQuery(`SELECT id FROM ${config.users_table} WHERE username='${uu}';`)
+                doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${uu}';`)
                     .then(rr => {
                         if (rr[0] === undefined) {
                             reject("Given user not found...")
@@ -133,7 +142,7 @@ module.exports = {
                         let i = r[0].id;
                         let ii = rr[0].id;
                         if (i <= ii)
-                            doQuery(`SELECT user_1_id, user_2_id FROM ${config.friendships_table} WHERE user_1_id=${i} AND user_2_id=${ii};`)
+                            doQuery(`SELECT user_1_id, user_2_id FROM ${tables_names.friendships_table} WHERE user_1_id=${i} AND user_2_id=${ii};`)
                                 .then((rows) => {
                                     resolve(rows)
                                 }).catch((err) => {
@@ -141,7 +150,7 @@ module.exports = {
                                     reject(err)
                                 })
                         else
-                            doQuery(`SELECT user_1_id, user_2_id FROM ${config.friendships_table} WHERE user_1_id=${ii} AND user_2_id=${i};`)
+                            doQuery(`SELECT user_1_id, user_2_id FROM ${tables_names.friendships_table} WHERE user_1_id=${ii} AND user_2_id=${i};`)
                                 .then((rows) => {
                                     resolve(rows)
                                 }).catch((err) => {
@@ -152,13 +161,13 @@ module.exports = {
             })
     }),
     getTemplates: (u) => new Promise((resolve, reject) => {
-        doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}';`)
+        doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}';`)
             .then(result => {
                 if (result[0] === undefined) {
                     reject("Given user not found...")
                     return
                 }
-                doQuery(`SELECT * FROM ${config.templates_table} WHERE owner_id=${result[0].id};`)
+                doQuery(`SELECT * FROM ${tables_names.templates_table} WHERE owner_id=${result[0].id};`)
                     .then((rows) => {
                         resolve(rows)
                     }).catch((err) => {
@@ -167,58 +176,58 @@ module.exports = {
                     })
             })
     }),
-    addUser: (l, p, u) => doQuery(`INSERT INTO ${config.users_table} (login, password, username, activated) VALUES('${l}','${p}','${u}','F');`),
+    addUser: (l, p, u) => doQuery(`INSERT INTO ${tables_names.users_table} (login, password, username, activated) VALUES('${l}','${p}','${u}','F');`),
     addMessage: (sender, recipient, c, d) => {
-        return doQuery(`SELECT id FROM ${config.users_table} WHERE username='${sender}'`).then(sender_id => {
-            doQuery(`SELECT id FROM ${config.users_table} WHERE username='${recipient}'`).then(recipient_id => {
-                doQuery(`INSERT INTO ${config.messages_table} (sender_id, recipient_id, content, m_date) VALUES(${sender_id[0].id},${recipient_id[0].id},'${c}','${d}');`)
+        return doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${sender}'`).then(sender_id => {
+            doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${recipient}'`).then(recipient_id => {
+                doQuery(`INSERT INTO ${tables_names.messages_table} (sender_id, recipient_id, content, m_date) VALUES(${sender_id[0].id},${recipient_id[0].id},'${c}','${d}');`)
             })
         })
     },
-    addCode: (c, u, t = 'T') => doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}';`).then((u_id) => {
-        doQuery(`INSERT INTO ${config.codes_table} (code, user_id,temporary) VALUES ('${c}',${u_id[0].id},'${t}');`)
+    addCode: (c, u, t = 'T') => doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}';`).then((u_id) => {
+        doQuery(`INSERT INTO ${tables_names.codes_table} (code, user_id,temporary) VALUES ('${c}',${u_id[0].id},'${t}');`)
     }),
     addNotification: (u, uu, t = "FRIEND_REQUEST") => {
-        return doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}'`).then(user_1_id => {
-            doQuery(`SELECT id FROM ${config.users_table} WHERE username='${uu}'`).then(user_2_id => {
-                doQuery(`INSERT INTO ${config.notifications_table} (user_1_id, user_2_id, type) VALUES(${user_1_id[0].id}, ${user_2_id[0].id}, '${t}');`)
+        return doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}'`).then(user_1_id => {
+            doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${uu}'`).then(user_2_id => {
+                doQuery(`INSERT INTO ${tables_names.notifications_table} (user_1_id, user_2_id, type) VALUES(${user_1_id[0].id}, ${user_2_id[0].id}, '${t}');`)
             })
         })
     },
     addFriendship: (u, uu) => {
-        return doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}'`).then(user_1_id => {
-            doQuery(`SELECT id FROM ${config.users_table} WHERE username='${uu}'`).then(user_2_id => {
+        return doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}'`).then(user_1_id => {
+            doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${uu}'`).then(user_2_id => {
                 let i = user_1_id[0].id;
                 let ii = user_2_id[0].id;
                 if (i <= ii)
-                    doQuery(`INSERT INTO ${config.friendships_table} (user_1_id, user_2_id) VALUES(${i}, ${ii});`);
+                    doQuery(`INSERT INTO ${tables_names.friendships_table} (user_1_id, user_2_id) VALUES(${i}, ${ii});`);
                 else
-                    doQuery(`INSERT INTO ${config.friendships_table} (user_1_id, user_2_id) VALUES(${ii}, ${i});`);
+                    doQuery(`INSERT INTO ${tables_names.friendships_table} (user_1_id, user_2_id) VALUES(${ii}, ${i});`);
             })
         })
     },
-    activateUser: (u) => doQuery(`UPDATE ${config.users_table} SET activated='T' WHERE username='${u}';`),
-    changePass: (u, p) => doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}';`).then((u_id) => {
-        doQuery(`UPDATE ${config.users_table} SET password='${p}' WHERE id=${u_id[0].id};`)
+    activateUser: (u) => doQuery(`UPDATE ${tables_names.users_table} SET activated='T' WHERE username='${u}';`),
+    changePass: (u, p) => doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}';`).then((u_id) => {
+        doQuery(`UPDATE ${tables_names.users_table} SET password='${p}' WHERE id=${u_id[0].id};`)
     }),
-    deleteCode: (code, t = 'T') => doQuery(`DELETE FROM ${config.codes_table} WHERE code=${code} AND temporary='${t}';`),
+    deleteCode: (code, t = 'T') => doQuery(`DELETE FROM ${tables_names.codes_table} WHERE code=${code} AND temporary='${t}';`),
     deleteFriend: (u, uu) => {
-        return doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}'`).then(user_1_id => {
-            doQuery(`SELECT id FROM ${config.users_table} WHERE username='${uu}'`).then(user_2_id => {
+        return doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}'`).then(user_1_id => {
+            doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${uu}'`).then(user_2_id => {
                 let i = user_1_id[0].id;
                 let ii = user_2_id[0].id;
                 if (i <= ii)
-                    doQuery(`DELETE FROM ${config.friendships_table} WHERE user_1_id=${i} AND user_2_id=${ii};`)
+                    doQuery(`DELETE FROM ${tables_names.friendships_table} WHERE user_1_id=${i} AND user_2_id=${ii};`)
                 else
-                    doQuery(`DELETE FROM ${config.friendships_table} WHERE user_1_id=${ii} AND user_2_id=${i};`)
+                    doQuery(`DELETE FROM ${tables_names.friendships_table} WHERE user_1_id=${ii} AND user_2_id=${i};`)
             })
         })
     },
-    deleteNotificationById: (i) => doQuery(`DELETE FROM ${config.notifications_table} WHERE id=${i};`),
+    deleteNotificationById: (i) => doQuery(`DELETE FROM ${tables_names.notifications_table} WHERE id=${i};`),
     deleteNotification: (u, uu) => {
-        return doQuery(`SELECT id FROM ${config.users_table} WHERE username='${u}'`).then(user_1_id => {
-            doQuery(`SELECT id FROM ${config.users_table} WHERE username='${uu}'`).then(user_2_id => {
-                doQuery(`DELETE FROM ${config.notifications_table} WHERE user_1_id=${user_1_id[0].id} AND user_2_id=${user_2_id[0].id};`)
+        return doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${u}'`).then(user_1_id => {
+            doQuery(`SELECT id FROM ${tables_names.users_table} WHERE username='${uu}'`).then(user_2_id => {
+                doQuery(`DELETE FROM ${tables_names.notifications_table} WHERE user_1_id=${user_1_id[0].id} AND user_2_id=${user_2_id[0].id};`)
             })
         })
     }
